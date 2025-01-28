742032379f38d9f60ba1565a62b72acec36d09c6
dhruv-ojha-02 <dhruvojha02@gmail.com>
2025-01-28 12:41:32 +0530

    Week 3 updated

--- Week3/prototype.js
+++ Week3/prototype.js
@@ -36,3 +36,46 @@ user.prototype.printName = function (){
 
 let user1 = new user ("Dhruv", 22);
 user1.printName();
+
+
+
+
+// Added a custom method to all String objects using String.prototpe: 
+
+let anotherUsername = "HelloDhruv     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`); //returns the current context
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"dhruv    ".trueLength()
+"ojha ".trueLength()
+
+
+
+// Impelementation of inheritance using prototypesx
+
+let dog = {
+    bark: true,
+    legs: 4
+}
+
+let labrador = {
+    color: "brown",
+    name: "Luffy"
+}
+
+
+// To inherit the properties of 'Dog' to 'labrador object,
+// we can use Object.setPrototypeOf(obj, prototype);
+
+Object.setPrototypeOf(labrador,dog);
+
+console.log(labrador.bark);
+console.log(labrador.legs);
+console.log(labrador.color);
+
+
+// It can be observed that 'labrador' has inherited the properties of 'dog', thus illustrating inheritance in JS
